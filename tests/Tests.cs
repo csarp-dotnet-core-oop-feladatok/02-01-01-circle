45e69a9e2d11bfe887acbe7eeb7d0490c8cb80a1
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-23 14:33:18 +0100

    Teszt javítás

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -29,13 +29,25 @@ namespace CircleProject.Tests
             Assert.AreEqual(expectedRadius, actualRadius, 0.01,"A Circle Radius tulajdonság set vagy get metódusa nem megfelelően működik!");
 
             double expectedDistrict = 32.9867;
-            double actualDistrict = c1.District;
+            double actualDistrict = c1.Perimeter;
             Assert.AreEqual(expectedDistrict, actualDistrict, 0.01, "A Circle District tulajdonság get metódusa nem megfelelően működik!");
 
             double expectedArea = 86.5901;
             double actualArea = c1.Area;
             Assert.AreEqual(expectedArea, actualArea,0.01, "A Circle Area tulajdonság get metódusa nem megfelelően működik!");
 
+            string toString = c1.ToString();
+            string radius = c1.Radius.ToString();
+            string roundedPrerimeter= Math.Round(c1.Perimeter, 2).ToString() ;
+            string roundedArea = Math.Round(c1.Area, 2).ToString();
+
+            bool haveRadius = toString.Contains(radius);
+            bool haveParimeter = toString.Contains(roundedPrerimeter);
+            bool haveArea = toString.Contains(roundedArea);
+
+            Assert.IsTrue(haveRadius, "A ToString metódus nem jeleníti meg a sugarat!");
+            Assert.IsTrue(haveParimeter, "A ToString metódus nem jeleníti meg a kerületet két tizedes jegy pontossággal!");
+            Assert.IsTrue(haveArea, "A ToString metódus nem jeleníti meg a területet két tizedes jegy pontosságal.");
         }
     }
 }
